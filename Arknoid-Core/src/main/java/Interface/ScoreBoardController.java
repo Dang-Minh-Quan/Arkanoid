2ad4d6db82e3bb9d53c7f33f8e2faffdcd55f7af
Dang-Minh-Quan <minhquanzoro@gmail.com>
2025-10-05 16:18:38 +0700

    add code

---
+++ Arknoid-Core/src/main/java/Interface/ScoreBoardController.java
@@ -0,0 +1,32 @@
+package Interface;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+public class ScoreBoardController {
+
+  @FXML
+  private Button ButtonBack;
+
+  @FXML
+  protected void Back(ActionEvent event) throws IOException {
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    Parent root = FXMLLoader.load(getClass().getResource("/Interface/MainMenu.fxml"));
+    Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.show();
+
+    System.out.println("Clicked Back");
+  }
+}
