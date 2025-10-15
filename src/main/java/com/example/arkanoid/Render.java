2d277c8a781a4154f8ddeb5a72ee2762b318395d
thieuhung123 <thieuhung1172006@gmail.com>
2025-10-16 00:35:07 +0700

    update Render.java Update.java

--- src/main/java/com/example/arkanoid/Render.java
+++ src/main/java/com/example/arkanoid/Render.java
@@ -3,16 +3,32 @@ package com.example.arkanoid;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
 
 import static com.example.arkanoid.Specifications.*;
 
 public class Render {
-    public void renderGame(Paddle paddle, Brick[][] brick, Pane pane) {
+    public void renderGame(ArrayList<Ball>balls,Ball ball, Paddle paddle, Brick[][] brick, Pane pane, ArrayList<PowerUp> powerUps) {
         pane.getChildren().clear();
         renderBackGround(pane);
         renderBrick(brick, pane);
         renderPaddle(paddle, pane);
+        renderBall(ball,pane);
+        renderBalls(balls,pane);
+        renderPowerUp(powerUps,pane);
+    }
+
+    private void renderPowerUp(ArrayList<PowerUp> powerUps, Pane pane) {
+        for (int i=0;i<powerUps.size();i++) {
+            Rectangle powerUpImage = new Rectangle(powerUps.get(i).x, powerUps.get(i).y,
+                    powerUps.get(i).width, powerUps.get(i).height);
+            powerUpImage.setFill(Color.BLACK);
+            powerUpImage.setStroke(Color.BLUE);
+            pane.getChildren().add(powerUpImage);
+        }
     }
 
     private void renderBackGround(Pane pane) {
@@ -28,10 +44,20 @@ public class Render {
     }
 
     private void renderPaddle(Paddle paddle, Pane pane) {
-        Rectangle paddleImage = new Rectangle(paddle.x - paddle.height / 2, paddle.y,
+        Rectangle paddleImage = new Rectangle(paddle.x , paddle.y,
                 paddle.width, paddle.height);
         paddleImage.setFill(Color.BLACK);
         paddleImage.setStroke(Color.BLUE);
         pane.getChildren().add(paddleImage);
+    }
+
+    private void renderBall(Ball ball, Pane pane){
+        pane.getChildren().addAll(ball.getBall());
+    }
+
+    private void renderBalls(ArrayList<Ball>balls, Pane pane) {
+        for (int i = 0; i < balls.size(); i++) {
+            pane.getChildren().addAll(balls.get(i).getBall());
+        }
     }
 }
--- src/main/java/com/example/arkanoid/Update.java
+++ src/main/java/com/example/arkanoid/Update.java
@@ -1,39 +1,48 @@
 package com.example.arkanoid;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.example.arkanoid.Specifications.*;
 
 public class Update {
-    private static void updatePaddle(Paddle paddle) {
-        paddle.Update();
-    }
 
-    public void updateGame(Paddle paddle, Brick[][] brick, AtomicInteger Level) {
-        updateBrick(Level, brick);
+    public void updateGame(ArrayList<Ball>balls,Ball ball, Paddle paddle, Brick[][] brick, AtomicInteger Level, ArrayList<PowerUp> powerUps) {
+        updatePowerUp(balls,ball,paddle,powerUps);
+        updateBall(ball,brick,paddle,powerUps);
+        updateBalls(balls,brick,paddle,powerUps);
+        updateBrick(balls,ball,paddle,Level, brick);
         updatePaddle(paddle);
     }
 
-    private void updateBrick(AtomicInteger Level, Brick[][] brick) {
-        System.out.println(Level);
+    private void updatePowerUp(ArrayList<Ball>balls,Ball ball, Paddle paddle, ArrayList<PowerUp> powerUps){
+        for(int i=0;i<powerUps.size();i++){
+            if(powerUps.get(i).UpdatePU(balls,paddle,ball)){
+                powerUps.remove(i);
+            }
+        }
+    }
+
+    private void updateBrick(ArrayList<Ball>balls,Ball ball,Paddle paddle,AtomicInteger Level, Brick[][] brick) {
         if (numBrick <= 0) {
             Level.getAndIncrement();
             if (Level.get() <= LevelMax) {
-                builderLevel(brick, Level);
+                builderLevel(balls,ball,brick,paddle, Level);
             } else {
                 Level.set(0);
                 //WIN
             }
         } else {
-            numBrick--;
+            //numBrick--;
         }
     }
 
-    private void builderLevel(Brick[][] brick, AtomicInteger Level) {
+    private void builderLevel(ArrayList<Ball>balls,Ball ball ,Brick[][] brick, Paddle paddle,AtomicInteger Level) {
         Map map = new Map();
         if (Level.get() > LevelMax) {
             checkPlay = false;
         } else {
+            resert(ball,paddle,balls);
             int[][] a = map.builderMap(Level.get());
             for (int i = 0; i < ROW; i++) {
                 for (int j = 0; j < COL; j++) {
@@ -47,4 +56,92 @@ public class Update {
             }
         }
     }
+    private static void updatePaddle(Paddle paddle) {
+        paddle.Update();
+    }
+
+    private void resert(Ball ball,Paddle paddle,ArrayList<Ball>balls){
+        ball.x=500;
+        ball.y=400;
+        paddle.width=paddleWidthOriginal;
+        balls.clear();
+    }
+
+    private static void updateBall(Ball ball,Brick[][] brick,Paddle paddle,ArrayList<PowerUp>powerUps){
+        ball.setBall(ball.x + ball.vx, ball.y + ball.vy);
+        switch(ball.checkWallCollision()) {
+            case 1:
+                ball.vx = -ball.vx;
+                ball.vy = -ball.vy;
+                break;
+            case 2:
+                ball.vx = -ball.vx;
+                break;
+            case 3:
+                ball.vy = -ball.vy;
+                break;
+        }
+
+        switch (ball.checkPaddleCollision(paddle)) {
+            case 1:
+                ball.vy = -ball.vy;
+                break;
+            case 2:
+                ball.vx = -ball.vx;
+                break;
+            case 3:
+                ball.vx = -ball.vx;
+                break;
+        }
+
+        switch (ball.checkBrickCollision(brick,powerUps)){
+            case 1:
+                ball.vx = -ball.vx;
+                break;
+            case 2:
+                ball.vy = -ball.vy;
+                break;
+        }
+    }
+
+
+    private static void updateBalls(ArrayList<Ball>balls,Brick[][] brick,Paddle paddle,ArrayList<PowerUp>powerUps) {
+        for (int i = 0; i < balls.size(); i++) {
+            balls.get(i).setBall(balls.get(i).x + balls.get(i).vx, balls.get(i).y + balls.get(i).vy);
+            switch (balls.get(i).checkWallCollision()) {
+                case 1:
+                    balls.get(i).vx = -balls.get(i).vx;
+                    balls.get(i).vy = -balls.get(i).vy;
+                    break;
+                case 2:
+                    balls.get(i).vx = -balls.get(i).vx;
+                    break;
+                case 3:
+                    balls.get(i).vy = -balls.get(i).vy;
+                    break;
+            }
+
+            switch (balls.get(i).checkPaddleCollision(paddle)) {
+                case 1:
+                    balls.get(i).vy = -balls.get(i).vy;
+                    break;
+                case 2:
+                    balls.get(i).vx = -balls.get(i).vx;
+                    break;
+                case 3:
+                    balls.get(i).vx = -balls.get(i).vx;
+                    break;
+            }
+
+            switch (balls.get(i).checkBrickCollision(brick, powerUps)) {
+                case 1:
+                    balls.get(i).vx = -balls.get(i).vx;
+                    break;
+                case 2:
+                    balls.get(i).vy = -balls.get(i).vy;
+                    break;
+            }
+        }
+    }
+
 }
