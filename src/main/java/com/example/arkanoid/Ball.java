10d96213a97d6178207cc3dc5d93e6d3c7518bf6
Dinh Van Duong <thaiduongdinh5@gmail.com>
2025-10-15 22:47:26 +0700

    brick collision update

--- src/main/java/com/example/arkanoid/Ball.java
+++ src/main/java/com/example/arkanoid/Ball.java
@@ -10,9 +10,10 @@ public class Ball extends BaseClass {
     private boolean collidedWithPaddle = false;
 
     public Ball(){
-        super(null, 0, 500, 400, spvxOriginal, spvxOriginal, 10, 10);
+        super(null, 0, 450, HEIGHT - 70, spvxOriginal, spvxOriginal, 10, 10);
         ball = new Circle(x, y, width, Color.BLUE);
     }
+
     public void setBall(double dx, double dy) {
         x = dx;
         y = dy;
@@ -77,5 +78,100 @@ public class Ball extends BaseClass {
             collidedWithPaddle = false;
         }
         return false;
+    }
+
+    public int checkBrickCollision(Brick[][] brick) {
+        int brickCol = (int)x / (int)WIDTHBrick ;
+        int brickRow = (int)y / (int)HEIGHTBrick ;
+
+        boolean checkX = false, checkY = false, top = false, below = false, left = false, right = false;
+
+        if (vy < 0 && brickRow > 0 && brickRow < ROW && (brickRow) * HEIGHTBrick + width >= y) {
+            top = true;
+        }
+        if (vy > 0 && brickRow < ROW - 1 && (brickRow + 1) * HEIGHTBrick - width <= y) {
+            below = true;
+        }
+        if (vx < 0 && brickCol > 0 && brickRow < ROW && (brickCol) * WIDTHBrick + width >= x) {
+            left = true;
+        }
+        if (vx > 0 && brickCol < COL - 1 && brickRow < ROW && (brickCol + 1) * WIDTHBrick - width <= x) {
+            right = true;
+        }
+
+        if (top == true) {
+            if (brick[brickRow - 1][brickCol].type != 0) {
+                brick[brickRow - 1][brickCol].UpdateBrick(this);
+                checkY = true;
+            }
+        }
+        if (below == true) {
+            if (brick[brickRow + 1][brickCol].type != 0) {
+                brick[brickRow + 1][brickCol].UpdateBrick(this);
+                checkY = true;
+            }
+        }
+        if (left == true){
+            if (brick[brickRow][brickCol - 1].type != 0) {
+                brick[brickRow][brickCol - 1].UpdateBrick(this);
+                checkX = true;
+            }
+        }
+        if (right == true){
+            if(brick[brickRow][brickCol + 1].type != 0) {
+                brick[brickRow][brickCol + 1].UpdateBrick(this);
+                checkX = true;
+            }
+        }
+        if (checkX == true && checkY == false) {
+            return 1;
+        }
+        if (checkX == false && checkY == true) {
+            return 2;
+        }
+        if (checkX == true && checkY == true) {
+            return 3;
+        }
+        if (top == true && left == true) {
+            if (brick[brickRow - 1][brickCol - 1].type != 0) {
+                brick[brickRow - 1][brickCol - 1].UpdateBrick(this);
+                if (Math.abs((brickRow) * HEIGHTBrick - (int)y) > Math.abs((brickCol) * WIDTHBrick - (int)x)) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        }
+        if (below == true && left == true) {
+            if (brick[brickRow + 1][brickCol - 1].type != 0) {
+                brick[brickRow + 1][brickCol - 1].UpdateBrick(this);
+                if (Math.abs((brickRow + 1) * HEIGHTBrick - (int)y) > Math.abs((brickCol) * WIDTHBrick - (int)x)) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        }
+        if (top == true && right == true) {
+            if (brick[brickRow - 1][brickCol + 1].type != 0) {
+                brick[brickRow - 1][brickCol + 1].UpdateBrick(this);
+                if (Math.abs((brickRow) * HEIGHTBrick - (int)y) > Math.abs((brickCol + 1) * WIDTHBrick - (int)x)) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        }
+        if (below == true && right == true) {
+            if (brick[brickRow + 1][brickCol + 1].type != 0) {
+                brick[brickRow + 1][brickCol + 1].UpdateBrick(this);
+                if (Math.abs((brickRow + 1) * HEIGHTBrick - (int)y) > Math.abs((brickCol + 1) * WIDTHBrick - (int)x)) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        }
+        return 0;
     }
 }
