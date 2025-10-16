544dcbf82ae03bdc0fc574a78e5667fe6fa0a9a9
thieuhung123 <thieuhung1172006@gmail.com>
2025-10-16 16:36:23 +0700

    update PowerUp.java

--- src/main/java/com/example/arkanoid/PowerUp.java
+++ src/main/java/com/example/arkanoid/PowerUp.java
@@ -1,35 +1,50 @@
 package com.example.arkanoid;
 
-import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 import static com.example.arkanoid.Specifications.*;
 
-public class PowerUp extends baseClass{
-    public PowerUp(double i,double j){
-        super(null, i, j,0,speedPU, WIDTHPU,HEIGHTPU);
+public class PowerUp extends Baseclass{
+    private Circle HitBoxPowerUp;
+
+    public PowerUp(int i,int j){
+        super( i+WIDTHBrick/2, j+HEIGHTBrick/2,0,speedPU, RADIUSPU,RADIUSPU);
         type = (int)(Math.random()*PU)%PU;
+        HitBoxPowerUp = new Circle(x, y, width, Color.BLACK);
     }
 
-    public boolean UpdatePU(Paddle paddle,Ball ball) {
-        y=y+vy;
+    public Circle getHitBoxPowerUp() {
+        return HitBoxPowerUp;
+    }
 
-        if(x >= paddle.x-width&&x<=paddle.x+paddle.width&&y>=paddle.y-height&&y<=paddle.y+paddle.height){
-            Activate(paddle,ball);
+    public boolean UpdatePU(ArrayList<Ball>balls, Paddle paddle, Ball ball) {
+        y=y+vy;
+        HitBoxPowerUp.setCenterY(y);
+        if(Shape.intersect(HitBoxPowerUp,paddle.getHitBoxPaddle()).getBoundsInLocal().getWidth()>0){
+            Activate(balls,paddle,ball);
             return true;
         }
 
-        if(y==HEIGHT){return true;}
+        if(y==HEIGHT+RADIUSPU){return true;}
         return false;
     }
 
-    public void Activate(Paddle paddle,Ball ball){
+    public void Activate(ArrayList<Ball>balls,Paddle paddle,Ball ball){
+        System.out.println(type);
         if(type==0){
-            ball.UpdateWidth();
+
         }
         if (type==1){
             paddle.width=300;
+        }
+        if (type==2){
+            Ball newBall=new Ball();
+            balls.add(newBall);
         }
     }
 }
