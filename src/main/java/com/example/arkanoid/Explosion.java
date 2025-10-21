caddd67e569670873c37d58e54f1b9272080840b
phuongngoc16 <24020253@vnu.edu.vn>
2025-10-22 00:19:11 +0700

    update

--- src/main/java/com/example/arkanoid/Explosion.java
+++
@@ -1,9 +0,0 @@
-package com.example.arkanoid;
-
-import javafx.scene.image.Image;
-
-public class Explosion extends AnimationClass {
-    public Explosion(int x, int y) {
-        super(MainImage.getExplosion(), x, y, 4, 4, 64, 64, 10);
-    }
-}
