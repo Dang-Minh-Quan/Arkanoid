698aa752e9fa86b7acaf24f64a243e3853a291ba
phuongngoc16 <24020253@vnu.edu.vn>
2025-10-30 10:57:42 +0700

    update PowerUp

--- src/main/java/LogicGamePlay/PowerUp.java
+++ src/main/java/LogicGamePlay/PowerUp.java
@@ -1,38 +1,4 @@
-/*package LogicGamePlay;
-
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import static LogicGamePlay.Specifications.*;
-
-public class PowerUp {
-    public double x, y;
-    public double width = 30, height = 30;
-    public double vy = 2;
-    public boolean active = true;
-    public Image image;
-
-    public PowerUp(double x, double y, Image image) {
-        this.x = x;
-        this.y = y;
-        this.image = image;
-    }
-
-    public void update() {
-        if (!active) return;
-        y += vy;
-
-        if (y > HEIGHT) {
-            active = false;
-        }
-    }
-
-    public void render(GraphicsContext gc) {
-        if (!active || image == null) return;
-        gc.drawImage(image, x, y, width, height);
-    }
-}
-*/
-package LogicGamePlay;
+ package LogicGamePlay;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
