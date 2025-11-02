bf75ce273aa3130ba5bdb12f7a032145dbb3c30e
Dinh Van Duong <thaiduongdinh5@gmail.com>
2025-11-02 23:30:21 +0700

    clean up and update project

--- src/main/java/LogicGamePlay/Map.java
+++ src/main/java/LogicGamePlay/Map.java
@@ -11,19 +11,19 @@ public class Map {
         String path = new String();
         switch (Level) {
             case 1:
-                path= "/Interface/Map/Level1.txt";
+                path = "/Interface/Map/Level1.txt";
                 break;
             case 2:
-                path= "/Interface/Map/Level2.txt";
+                path = "/Interface/Map/Level2.txt";
                 break;
             case 3:
-                path= "/Interface/Map/Level3.txt";
+                path = "/Interface/Map/Level3.txt";
                 break;
         }
         InputStream input = getClass().getResourceAsStream(path);
-        try(Scanner sc=new Scanner(input)) {
-            for (int i=0;i<ROW;i++){
-                for (int j=0;j<COL;j++){
+        try (Scanner sc = new Scanner(input)) {
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
                     a[i][j] = sc.nextInt();
                 }
             }
--- src/main/java/LogicGamePlay/PowerUp.java
+++ src/main/java/LogicGamePlay/PowerUp.java
@@ -109,4 +109,3 @@ public class PowerUp extends BaseClass {
         return active;
     }
 }
-
