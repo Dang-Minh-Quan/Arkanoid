b8faebac4146ed8db06561dc98849ff16939f045
Dinh Van Duong <thaiduongdinh5@gmail.com>
2025-11-12 02:02:41 +0700

    fix collision ball with paddle

--- src/main/java/Ball/Ball.java
+++ src/main/java/Ball/Ball.java
@@ -156,8 +156,8 @@ public abstract class Ball extends BaseClass {
         if (!collisionX || !collisionY) {
             return -1;
         }
-        if ((ballX < paddleLeft - radius / 2 && ballX + vx < paddleLeft - radius / 2)
-                || (ballX > paddleRight + radius / 2 && ballX + vx > paddleRight + radius / 2)) {
+        if ((ballX < paddleLeft - radius / 2 && ballX - vx < paddleLeft - radius / 2)
+                || (ballX > paddleRight + radius / 2 && ballX - vx > paddleRight + radius / 2)) {
             return 2;
         }
         return 1;
